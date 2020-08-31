b2d0d631ebd9ee609140753a451caacd23df55cb
Antonio_batista <antonio.batista@techwayinfo.com.br>
2020-08-31 10:51:24 -0300

    paginas e zoom

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 function moduloNoturno(dadosPagina) { 
          var a = dadosPagina.getElementById('botao')
          var corpo = dadosPagina.getElementsByTagName('body')[0]
-         console.log(corpo)
+        
          blackmode = 'Desligado'
          
          a.addEventListener('click', clicar)
          
          function clicar()
          {
+          const nav = document.querySelector('.navbar')
+          const subnav = document.querySelector('.nav-light')
            if(blackmode == 'Desligado')
            {
              corpo.style.background = 'black'
@@ -16,6 +18,10 @@ function moduloNoturno(dadosPagina) {
              a.style.color = 'white'
              blackmode = 'Ligado'
              a.innerText = 'Modo Normal'
+             nav.classList.remove('bg-primary')
+             nav.classList.add('bg-dark')
+             subnav.style.backgroundColor = '#484c4f';
+              document.querySelector('html').classList.add('night')
            }
            else if(blackmode == 'Ligado')
            {
@@ -25,7 +31,47 @@ function moduloNoturno(dadosPagina) {
              a.style.color = 'white'
              blackmode = 'Desligado'
              a.innerText = 'Modo Noturno'
+             nav.classList.remove('bg-dark')
+             nav.classList.add('bg-primary')
+             subnav.style.backgroundColor = '#e3f2fd';
+             document.querySelector('html').classList.remove('night')
+
            }  
            
          } 
-  }
+ }
+
+function zoom(dadosPagina) {
+	var btnMais = dadosPagina.getElementById('btn-zoom-positivo')
+	var btnMenos = dadosPagina.getElementById('btn-zoom-negativo')
+	var corpo = dadosPagina.getElementsByTagName('body')[0]
+
+	btnMais.addEventListener('click', aumentarZoom)
+
+	btnMenos.addEventListener('click', diminuirZoom)
+	var contClickMais = 0
+	var contClickMenos = 0
+
+	function aumentarZoom() {
+		contClickMais = contClickMais + 1
+		contClickMenos = contClickMais
+		var valorAbsoluto = Math.abs(contClickMenos)
+		if (contClickMenos < 0) {
+			corpo.style.transform = 'scale(' + (1 - valorAbsoluto / 100) + ')'
+		} else {
+			corpo.style.transform = 'scale(' + (1 + contClickMais / 100) + ')'
+		} 
+	}
+
+	function diminuirZoom() {
+		contClickMenos = contClickMenos - 1 
+		contClickMais = contClickMenos
+		var valorAbsoluto = Math.abs(contClickMenos)
+		if (contClickMenos < 0) {
+			corpo.style.transform = 'scale(' + (1 - valorAbsoluto / 100) + ')'
+		} else {
+			corpo.style.transform = 'scale(' + (1 + contClickMenos / 100) + ')'
+		}
+	}
+}
+
